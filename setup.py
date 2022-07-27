7135657886392b7e4c131c8a6636055584510e77
Jiarui Xu <xvjiarui0826@gmail.com>
2022-07-27 13:45:01 -0700

    remove args for find_pacakges()

--- setup.py
+++ setup.py
@@ -6,8 +6,7 @@ setup(
     version="1.0",
     description="",
     author="OFA",
-    packages=find_packages(include=["ofa"]),
-    # git@github.com:xvjiarui/OFA-fairseq.git
+    packages=find_packages(),
     install_requires = [
   'fairseq @ git+ssh://git@github.com/xvjiarui/OFA-fairseq@main#egg=fairseq',
 ],
